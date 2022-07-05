382c7c249e3846717a5be664500fd6933b305c53
Zan Skamljic <zan.skamljic@equaleyes.com>
2022-07-05 15:57:05 +0200

    feat: add tests for options converter

--- rest-ahead-client/src/main/java/io/github/zskamljic/restahead/conversion/OptionsConverter.java
+++ rest-ahead-client/src/main/java/io/github/zskamljic/restahead/conversion/OptionsConverter.java
@@ -4,20 +4,29 @@ import io.github.zskamljic.restahead.client.requests.Verb;
 import io.github.zskamljic.restahead.client.responses.Response;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
-public class OptionsConverter {
+public final class OptionsConverter {
+    private OptionsConverter() {
+    }
+
     public static List<Verb> parseOptions(Response response) {
-        var values = response.headers().get("Allow");
-        return values.stream()
+        return response.headers().entrySet()
+            .stream()
+            .filter(e -> e.getKey().equalsIgnoreCase("allow"))
+            .map(Map.Entry::getValue)
+            .flatMap(Collection::stream)
             .map(allow -> allow.split(", "))
             .flatMap(Arrays::stream)
             .map(String::trim)
             .mapMulti((BiConsumer<String, Consumer<Verb>>) (value, consumer) -> {
                 if ("*".equals(value)) {
                     Arrays.stream(Verb.values()).forEach(consumer);
+                    return;
                 }
                 consumer.accept(Verb.valueOf(value));
             })
